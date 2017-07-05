69d6d91cc82e80d01a0fe35bffeae445c6b74883
Sonali <mailsonalidhru@gmail.com>
2017-07-05 09:14:59 -0700

    Updates

--- SATScore/SATScore.py
+++ SATScore/SATScore.py
@@ -9,29 +9,29 @@ def barlabel(b, i):
         ax[i].text(bar.get_x()+bar.get_width()/2., 0.90*height, 
             '%d' % int(height), color='white', ha='center', va='bottom')
 
-print('The highest average SAT Math score was {} and the lowest was {}.'.format(
+print('Highest average SAT Math score {} Lowest score {}.'.format(
       int(data['Average Score (SAT Math)'].max()), int(data['Average Score (SAT Math)'].min())))
 print('\t')
-print('The highest average SAT Reading score was {} and the lowest was {}.'.format(
+print('Highest average SAT Reading score {} Lowest Score {}.'.format(
       int(data['Average Score (SAT Reading)'].max()), int(data['Average Score (SAT Reading)'].min())))
 print('\t')
-print('The highest average SAT Writing score was {} and the lowest was {}.'.format(
+print('Highest average SAT Writing score {} Lowest {}.'.format(
       int(data['Average Score (SAT Writing)'].max()), int(data['Average Score (SAT Writing)'].min())))
 
-print("The school with the best overall average SAT math score was {}.".format(
+print("Best School with Overall Average SAT Math Score {}.".format(
       data.loc[data['Average Score (SAT Math)']==754, 'School Name'].values[0]))
 print('\t')
-print("The school with the worst overall average SAT math score was {}".format(
+print("Worst School with Overall Average SAT Math Score {}".format(
       data.loc[data['Average Score (SAT Math)']==317, 'School Name'].values[0]))
 
-print("The school with the best overall average SAT reading score was {}.".format(
+print("Best School with Overall Average SAT Reading Score {}.".format(
       data.loc[data['Average Score (SAT Reading)']==697, 'School Name'].values[0]))
 print('\t')
-print("The school with the worst overall average SAT reading score was {}".format(
+print("Worst School with Overall Average SAT Reading Score {}".format(
       data.loc[data['Average Score (SAT Reading)']==302, 'School Name'].values[0]))
 
-print("The school with the best overall average SAT writing score was {}.".format(
+print("Best Shool with Overall Average SAT Writing Score {}.".format(
       data.loc[data['Average Score (SAT Writing)']==693, 'School Name'].values[0]))
 print('\t')
-print("The school with the worst overall average SAT writing score was {}".format(
+print("Worst School with Overall Average SAT Writing Score was {}".format(
       data.loc[data['Average Score (SAT Writing)']==284, 'School Name'].values[0]))
